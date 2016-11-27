0ec590e64ccbe0640dd2a2dfd018d54a41ba9230
VasilStamatov <vasil_0000@abv.bg>
2016-11-27 10:01:18 +0000

    Added scenes for levels 1 to 7 Added behavior to the level selector in the main menu and the levels in the selector Made prefabs for the rest of the parallax backgrounds deleted unneeded scenes (given by assignment)

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -15,10 +15,49 @@ public class MenuController : MonoBehaviour
   }
 
   /*
-   * When the back button is clicked, load the Menu scene.
+   * When the back button is clicked, load the main menus scene.
    */
   public void OnBackClicked()
   {
-				SceneManager.LoadScene("Menu");
+				SceneManager.LoadScene("MainMenu");
+		}
+
+		public void OnExitClicked()
+		{
+				Application.Quit();
+		}
+		public void OnLevelSelector()
+		{
+				SceneManager.LoadScene("LevelSelector");
+		}
+
+		/* Level selectors */
+		public void OnLevel1()
+		{
+				SceneManager.LoadScene("Level1");
+		}
+		public void OnLevel2()
+		{
+				SceneManager.LoadScene("Level2");
+		}
+		public void OnLevel3()
+		{
+				SceneManager.LoadScene("Level3");
+		}
+		public void OnLevel4()
+		{
+				SceneManager.LoadScene("Level4");
+		}
+		public void OnLevel5()
+		{
+				SceneManager.LoadScene("Level5");
+		}
+		public void OnLevel6()
+		{
+				SceneManager.LoadScene("Level6");
+		}
+		public void OnLevel7()
+		{
+				SceneManager.LoadScene("Level7");
 		}
 }
